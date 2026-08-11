b1e39ed8c825987ec99b89d51639075a94121672
MiguelNgel20 <mricardom1@unicartagena.edu.co>
2026-08-11 14:08:22 -0500

    clases item y product

--- src/main/java/com/miguel/proyecto/springboot/models/Item.java
+++ src/main/java/com/miguel/proyecto/springboot/models/Item.java
@@ -2,4 +2,22 @@ package com.miguel.proyecto.springboot.models;
 
 public class Item {
 
+    private Product product;
+    private int quantity;
+    
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+
 }
--- src/main/java/com/miguel/proyecto/springboot/models/Product.java
+++ src/main/java/com/miguel/proyecto/springboot/models/Product.java
@@ -1,5 +1,40 @@
 package com.miguel.proyecto.springboot.models;
 
+import java.util.Locale.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Product {
+    private int price;
+    private String name;
+
+    @Autowired
+    private com.miguel.proyecto.springboot.models.Category category;
+
+
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public com.miguel.proyecto.springboot.models.Category getCategory() {
+        return category;
+    }
+    public void setCategory(com.miguel.proyecto.springboot.models.Category category) {
+        this.category = category;
+    }
+
+
+    
+
+
+
 
 }
