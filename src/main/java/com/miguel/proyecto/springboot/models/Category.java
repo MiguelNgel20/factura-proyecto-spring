0de24ba72908ac8a770ad4fd525e8091974218a4
MiguelNgel20 <mricardom1@unicartagena.edu.co>
2026-08-11 14:07:30 -0500

    fix commit clases vacías

--- src/main/java/com/miguel/proyecto/springboot/models/Category.java
+++ src/main/java/com/miguel/proyecto/springboot/models/Category.java
@@ -1,5 +1,27 @@
 package com.miguel.proyecto.springboot.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Category {
+    private String name;
+    private String description;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    
 
 }
--- src/main/java/com/miguel/proyecto/springboot/models/Client.java
+++ src/main/java/com/miguel/proyecto/springboot/models/Client.java
@@ -1,5 +1,26 @@
 package com.miguel.proyecto.springboot.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Client {
+    private String name;
+    private String lastName;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    
 
 }
